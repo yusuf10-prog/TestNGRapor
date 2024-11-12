de221a270dfc5a6fcdea3b6284bf9eab89f165d5
YUSUF KUZYAKA <122876104+yusuf10-prog@users.noreply.github.com>
2024-11-12 16:20:00 +0300

    Update HomePage.java

--- src/test/java/pages/HomePage.java
+++ src/test/java/pages/HomePage.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import utilities.Driver;
-
+ 
 public class HomePage {
     public HomePage(){
         PageFactory.initElements(Driver.getDriver(),this);
